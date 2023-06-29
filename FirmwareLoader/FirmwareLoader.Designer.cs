bcc039e6c563475543405155ed6ea992ac0f8fe3
sicris <sicris.embay@gmail.com>
2023-06-29 11:18:47 +0800

    + Added CAN driver class to encapsulate PCAN

--- FirmwareLoader/FirmwareLoader.Designer.cs
+++ FirmwareLoader/FirmwareLoader.Designer.cs
@@ -28,20 +28,133 @@ namespace FirmwareLoader
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox_Connection = new System.Windows.Forms.GroupBox();
+            this.cbb_baudrates = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbb_channel = new System.Windows.Forms.ComboBox();
+            this.textBox_NodeID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_HwRefresh = new System.Windows.Forms.Button();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.groupBox_Connection.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox_Connection
+            // 
+            this.groupBox_Connection.Controls.Add(this.cbb_baudrates);
+            this.groupBox_Connection.Controls.Add(this.label3);
+            this.groupBox_Connection.Controls.Add(this.cbb_channel);
+            this.groupBox_Connection.Controls.Add(this.textBox_NodeID);
+            this.groupBox_Connection.Controls.Add(this.label2);
+            this.groupBox_Connection.Controls.Add(this.label1);
+            this.groupBox_Connection.Controls.Add(this.btn_HwRefresh);
+            this.groupBox_Connection.Controls.Add(this.btn_Connect);
+            this.groupBox_Connection.Location = new System.Drawing.Point(12, 21);
+            this.groupBox_Connection.Name = "groupBox_Connection";
+            this.groupBox_Connection.Size = new System.Drawing.Size(520, 121);
+            this.groupBox_Connection.TabIndex = 51;
+            this.groupBox_Connection.TabStop = false;
+            this.groupBox_Connection.Text = "Connection";
+            // 
+            // cbb_baudrates
+            // 
+            this.cbb_baudrates.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbb_baudrates.Location = new System.Drawing.Point(150, 43);
+            this.cbb_baudrates.Name = "cbb_baudrates";
+            this.cbb_baudrates.Size = new System.Drawing.Size(152, 21);
+            this.cbb_baudrates.TabIndex = 2;
+            this.cbb_baudrates.SelectedIndexChanged += new System.EventHandler(this.cbb_baudrates_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 49;
+            this.label3.Text = "Node ID:";
+            // 
+            // cbb_channel
+            // 
+            this.cbb_channel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbb_channel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbb_channel.Location = new System.Drawing.Point(22, 43);
+            this.cbb_channel.Name = "cbb_channel";
+            this.cbb_channel.Size = new System.Drawing.Size(119, 21);
+            this.cbb_channel.TabIndex = 1;
+            this.cbb_channel.SelectedIndexChanged += new System.EventHandler(this.cbb_channel_SelectedIndexChanged);
+            // 
+            // textBox_NodeID
+            // 
+            this.textBox_NodeID.Location = new System.Drawing.Point(70, 80);
+            this.textBox_NodeID.Name = "textBox_NodeID";
+            this.textBox_NodeID.Size = new System.Drawing.Size(42, 20);
+            this.textBox_NodeID.TabIndex = 3;
+            this.textBox_NodeID.Text = "10";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(147, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Baudrate:";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(19, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 15);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Channel:";
+            // 
+            // btn_HwRefresh
+            // 
+            this.btn_HwRefresh.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btn_HwRefresh.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_HwRefresh.Location = new System.Drawing.Point(310, 43);
+            this.btn_HwRefresh.Name = "btn_HwRefresh";
+            this.btn_HwRefresh.Size = new System.Drawing.Size(65, 57);
+            this.btn_HwRefresh.TabIndex = 46;
+            this.btn_HwRefresh.Text = "Refresh";
+            this.btn_HwRefresh.Click += new System.EventHandler(this.btn_HwRefresh_Click);
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Location = new System.Drawing.Point(381, 43);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(96, 57);
+            this.btn_Connect.TabIndex = 6;
+            this.btn_Connect.Text = "Connect";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
             // 
             // FirmwareLoader
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox_Connection);
             this.Name = "FirmwareLoader";
             this.Text = "Firmware Loader";
+            this.groupBox_Connection.ResumeLayout(false);
+            this.groupBox_Connection.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_Connection;
+        private System.Windows.Forms.ComboBox cbb_baudrates;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbb_channel;
+        private System.Windows.Forms.TextBox textBox_NodeID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_HwRefresh;
+        private System.Windows.Forms.Button btn_Connect;
     }
 }
 
--- FirmwareLoader/FirmwareLoader.cs
+++ FirmwareLoader/FirmwareLoader.cs
@@ -7,14 +7,167 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Peak.Can.Basic;
+using can;
 
 namespace FirmwareLoader
 {
+    using TPCANHandle = System.UInt16;
+
     public partial class FirmwareLoader : Form
     {
+        #region Members
+        private TPCANHandle pcanHandle;
+        private TPCANBaudrate m_baudrate;
+        private TPCANHandle[] m_HandlesArray;
+        private can_peak can_Peak = null;
+        #endregion // Members
+
+        #region Methods
         public FirmwareLoader()
         {
             InitializeComponent();
+
+            this.can_Peak = new can_peak();
+
+            this.m_HandlesArray = new TPCANHandle[]
+            {
+                PCANBasic.PCAN_USBBUS1,
+                PCANBasic.PCAN_USBBUS2,
+                PCANBasic.PCAN_USBBUS3,
+                PCANBasic.PCAN_USBBUS4,
+                PCANBasic.PCAN_USBBUS5,
+                PCANBasic.PCAN_USBBUS6,
+                PCANBasic.PCAN_USBBUS7,
+                PCANBasic.PCAN_USBBUS8,
+            };
+
+            this.set_obj_states(false);
+            this.btn_HwRefresh_Click(this, null);
         }
+
+        #region Helpers
+        private void set_obj_states(bool connected)
+        {
+            this.cbb_channel.Enabled = !connected;
+            this.cbb_baudrates.Enabled = !connected;
+            this.btn_HwRefresh.Enabled = !connected;
+            this.textBox_NodeID.Enabled = !connected;
+        }
+        #endregion
+
+        #region Message Handlers
+        private void CanRxHandler(object sender, CanRxMsgArgs e)
+        {
+            DateTime timestamp = DateTime.Now;
+            var ticks = timestamp.Ticks;
+
+            string rawStr = e.msgId.ToString("X4") + "h :";
+            for (int i = 0; i < e.data.Length; i++) {
+                rawStr += " " + e.data[i].ToString("X2") + "h";
+            }
+            rawStr += Environment.NewLine;
+
+            string logStr = ticks.ToString() + " rx " + e.msgId.ToString("X4");
+            for (int i = 0; i < e.data.Length; i++) {
+                logStr += " " + e.data[i].ToString("X2");
+            }
+            logStr += Environment.NewLine;
+
+            Console.WriteLine(logStr);
+        }
+
+        void CanTxHookHandler(object sender, CanRxMsgArgs e)
+        {
+            DateTime timestamp = DateTime.Now;
+            var ticks = timestamp.Ticks;
+
+            string logStr = ticks.ToString() + " tx " + e.msgId.ToString("X4");
+            if (e.data != null) {
+                for (int i = 0; i < e.data.Length; i++) {
+                    logStr += " " + e.data[i].ToString("X2");
+                }
+            }
+            logStr += Environment.NewLine;
+
+            Console.WriteLine(logStr);
+        }
+        #endregion // Message Handlers
+
+        #region Button Event Handlers
+        private void btn_HwRefresh_Click(object sender, EventArgs e)
+        {
+            UInt32 iBuffer;
+            TPCANStatus stsResult;
+
+            // Clears the Channel combioBox and fill it again with 
+            // the PCAN-Basic handles for no-Plug&Play hardware and
+            // the detected Plug&Play hardware
+            cbb_channel.Items.Clear();
+            cbb_baudrates.Items.Clear();
+            try {
+                for (int i = 0; i < m_HandlesArray.Length; i++) {
+                    // Includes all no-Plug&Play Handles
+                    if (( m_HandlesArray[i] >= PCANBasic.PCAN_USBBUS1 ) &&
+                        ( m_HandlesArray[i] <= PCANBasic.PCAN_USBBUS8 )) {
+                        stsResult = PCANBasic.GetValue(m_HandlesArray[i], TPCANParameter.PCAN_CHANNEL_CONDITION, out iBuffer, sizeof(UInt32));
+                        if (( stsResult == TPCANStatus.PCAN_ERROR_OK ) && ( iBuffer == PCANBasic.PCAN_CHANNEL_AVAILABLE )) {
+                            cbb_channel.Items.Add(string.Format("PCAN-USB{0}", m_HandlesArray[i] & 0x0F));
+                        }
+                    }
+                }
+                cbb_channel.SelectedIndex = cbb_channel.Items.Count - 1;
+            } catch (DllNotFoundException) {
+                MessageBox.Show("Unable to find the library: PCANBasic.dll !", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(-1);
+            }
+            cbb_baudrates.Items.Clear();
+            string[] baudName = Enum.GetNames(typeof(TPCANBaudrate));
+            for (int i = 0; i < baudName.Length; i++) {
+                cbb_baudrates.Items.Add(baudName[i]);
+            }
+            cbb_baudrates.SelectedIndex = 0;
+        }
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            try {
+                if (this.btn_Connect.Text == "Connect") {
+                    if (this.can_Peak.Connect(this.pcanHandle, this.m_baudrate)) {
+                        this.can_Peak.CanRxMsgEvent += this.CanRxHandler;
+                        this.can_Peak.CanTxHookEvent += this.CanTxHookHandler;
+
+                        this.set_obj_states(true);
+                        this.btn_Connect.Text = "Disconnect";
+                    }
+                } else {
+                    this.can_Peak.CanRxMsgEvent -= this.CanRxHandler;
+                    this.can_Peak.CanTxHookEvent -= this.CanTxHookHandler;
+                    this.can_Peak.Disconnect();
+
+                    this.set_obj_states(false);
+                    this.btn_Connect.Text = "Connect";
+                }
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion // Button Event Handlers
+
+        #region Combobox Event Handlers
+        private void cbb_channel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string strTemp = cbb_channel.Text;
+            strTemp = strTemp.Substring(strTemp.IndexOf('B') + 1, 1);
+            strTemp = "0x5" + strTemp;
+            this.pcanHandle = Convert.ToByte(strTemp, 16);
+        }
+        private void cbb_baudrates_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TPCANBaudrate[] baudValue = (TPCANBaudrate[])Enum.GetValues(typeof(TPCANBaudrate));
+            this.m_baudrate = baudValue[cbb_baudrates.SelectedIndex];
+        }
+        #endregion // Combobox Event Handlers
+
+        #endregion // Methods
     }
 }
